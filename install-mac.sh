5de0db3d80b2c1aac78199449bdbeae99c3ed3fd
tcao3 <Tri_Cao@intuit.com>
2018-06-17 17:04:32 -0500

    first commit

---
+++ install-mac.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+brew install openssl libuv cmake zlib
+git clone https://github.com/uWebSockets/uWebSockets 
+cd uWebSockets
+git checkout e94b6e1
+patch CMakeLists.txt < ../cmakepatch.txt
+mkdir build
+export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig 
+cd build
+OPENSSL_VERSION=`openssl version -v | cut -d' ' -f2`
+# cmake -DOPENSSL_ROOT_DIR=$(brew --cellar openssl)/$OPENSSL_VERSION -DOPENSSL_LIBRARIES=$(brew --cellar openssl)/$OPENSSL_VERSION/lib ..
+cmake -DOPENSSL_ROOT_DIR=$(brew --cellar openssl)/1.0.2o_2 -DOPENSSL_LIBRARIES=$(brew --cellar openssl)/1.0.2o_2/lib ..
+make 
+sudo make install
+cd ..
+cd ..
+sudo rm -r uWebSockets
