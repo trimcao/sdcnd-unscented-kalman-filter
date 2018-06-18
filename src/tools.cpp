d96617750d9ea17698559b83f81703786f7b3934
Tri M. Cao <trimcao@gmail.com>
2018-06-17 19:09:57 -0500

    added RMSE and initialization in ukf, not tested yet.

--- src/tools.cpp
+++ src/tools.cpp
@@ -12,7 +12,17 @@ Tools::~Tools() {}
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
   /**
-  TODO:
-    * Calculate the RMSE here.
+    * Calculate the RMSE.
   */
+  VectorXd rmse(4);
+  rmse.setZero();
+  VectorXd diff;
+  for (int i = 0; i < estimations.size(); i++) {
+    diff = estimations[i] - ground_truth[i];
+    diff = diff.array()*diff.array();
+    rmse += diff;
+  }
+  rmse = rmse / estimations.size();
+  rmse = rmse.array().sqrt();
+  return rmse;
 }
--- src/ukf.cpp
+++ src/ukf.cpp
@@ -12,6 +12,8 @@ using std::vector;
  * This is scaffolding, do not modify
  */
 UKF::UKF() {
+  
+  is_initialized_ = false;
   // if this is false, laser measurements will be ignored (except during init)
   use_laser_ = true;
 
@@ -20,15 +22,17 @@ UKF::UKF() {
 
   // initial state vector
   x_ = VectorXd(5);
+  x_.setZero();
 
   // initial covariance matrix
   P_ = MatrixXd(5, 5);
+  P_ = MatrixXd::Identity(5, 5);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 30;
+  std_a_ = 3; // set this to 3 initially
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 30;
+  std_yawdd_ = 3;
   
   //DO NOT MODIFY measurement noise values below these are provided by the sensor manufacturer.
   // Laser measurement noise standard deviation position1 in m
@@ -54,6 +58,15 @@ UKF::UKF() {
 
   Hint: one or more values initialized above might be wildly off...
   */
+
+  // time when the state is true?
+  time_us_ = 0;
+  Xsig_pred_.setZero();
+  // number of arguments 
+  n_x_ = 5;
+  n_aug_ = 7;
+  // sigma point spreading parameter
+  lambda_ = 3 - n_x_; // default vaule
 }
 
 UKF::~UKF() {}
@@ -69,6 +82,50 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   Complete this function! Make sure you switch between lidar and radar
   measurements.
   */
+
+  /*************
+  Initialization 
+  **************/
+  if (!is_initialized_) {
+    // set the timestamp
+    time_us_ = meas_package.timestamp_;
+    // initialize x 
+    if (meas_package.sensor_type_ == MeasurementPackage::LASER) {
+      x_ << meas_package.raw_measurements_(0), meas_package.raw_measurements_(1), 1, 1, 1;
+    }
+    else if (meas_package.sensor_type_ == MeasurementPackage::RADAR) {
+      double ro = meas_package.raw_measurements_(0);
+      double theta = meas_package.raw_measurements_(1);
+      x_ << ro*cos(theta), ro*sin(theta), 1, 1, 1;
+    }
+
+    // initialize P
+    P_ << 1, 0, 0, 0,
+          0, 1, 0, 0,
+          0, 0, 1, 0,
+          0, 0, 0, 1;
+
+    // done initialization
+    is_initialized_ = true;
+    return;
+  }
+
+  /*************
+  Prediction 
+  **************/
+  double delta_t = meas_package.timestamp_ - time_us_;
+  time_us_ = meas_package.timestamp_;
+  Prediction(delta_t);
+
+  /*************
+  Update
+  **************/
+  if (meas_package.sensor_type_ == MeasurementPackage::LASER) {
+    UpdateLidar(meas_package);
+  }
+  else if (meas_package.sensor_type_ == MeasurementPackage::RADAR) {
+    UpdateRadar(meas_package);
+  }
 }
 
 /**
